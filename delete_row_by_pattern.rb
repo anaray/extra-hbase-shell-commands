85f6a82de7861e35c0450ec9242fc283dd7325ba
anumodh <anumodh.n@metricstream.com>
2013-08-20 16:22:44 +0530

    adding check

--- delete_row_by_pattern.rb
+++ delete_row_by_pattern.rb
@@ -4,12 +4,12 @@ require "hbase/table"
 
 class Hbase::Table
 
-  def delete_by_pattern!(pattern, args = {})
-  		raise(ArgumentError, "pattern is null") if pattern.nil?
-        self.scan(args) do |row, cells|
-          self.delete(row, args['COLUMNS'])
-          puts "succesfully deleted row #{row}"
-        end
-  end
+  	def delete_by_pattern!(pattern, args = {})
+		raise(ArgumentError, "pattern is null") if pattern.nil?
+		self.scan(args) do |row, cells|
+ 			self.delete(row, args['COLUMNS'])
+			puts "succesfully deleted row #{row}"
+		end
+	end
 
 end
