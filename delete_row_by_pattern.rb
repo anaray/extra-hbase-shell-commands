0abd640a44b031ef0ed0f887b96a7b305bee59a4
anumodh <anumodh.n@metricstream.com>
2013-08-29 17:12:38 +0530

    refactoring

--- delete_row_by_pattern.rb
+++ delete_row_by_pattern.rb
@@ -1,14 +1,25 @@
 require "hbase"
 require "hbase/table"
 
+
 class Hbase::Table
 
-  def delete_by_pattern!(pattern, args = {})
-    raise(ArgumentError, "pattern is null") if pattern.nil?
-    self.scan(args) do |row, cells|
-      self.delete(row, args['COLUMNS'])
-      puts "succesfully deleted row #{row}"
-    end
-  end
+	def delete_row_by_pattern!(pattern,args = {})
+		raise(ArgumentError, "invalid pattern") if pattern.nil?
+		keyRegEx = org.apache.hadoop.hbase.filter.RegexStringComparator.new(pattern)
+		scan = org.apache.hadoop.hbase.client.Scan.new
+		scan.setFilter(org.apache.hadoop.hbase.filter.RowFilter.new(org.apache.hadoop.hbase.filter.CompareFilter::CompareOp::EQUAL, keyRegEx))
+
+		# Run the scanner
+		scanner = @table.getScanner(scan)
+		iterator = scanner.iterator
+		# Iterate results
+		while iterator.hasNext
+			row = iter.next
+			self.delete(row, args['COLUMNS'])
+			puts  "succesfully deleted row #{row}"
+		end
+	end
 
 end
+
