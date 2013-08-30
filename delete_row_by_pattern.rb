38d97a715955f1daeb496846be9e395e1f9af293
anumodh <anumodh.n@metricstream.com>
2013-08-30 15:10:05 +0530

    refactoring

--- delete_row_by_pattern.rb
+++ delete_row_by_pattern.rb
@@ -4,22 +4,24 @@ require "hbase/table"
 
 class Hbase::Table
 
-	def delete_row_by_pattern!(pattern,args = {})
-		raise(ArgumentError, "invalid pattern") if pattern.nil?
-		keyRegEx = org.apache.hadoop.hbase.filter.RegexStringComparator.new(pattern)
-		scan = org.apache.hadoop.hbase.client.Scan.new
-		scan.setFilter(org.apache.hadoop.hbase.filter.RowFilter.new(org.apache.hadoop.hbase.filter.CompareFilter::CompareOp::EQUAL, keyRegEx))
+        def delete_row_by_pattern!(regex_pattern,args = {})
+                raise(ArgumentError, "invalid pattern!") if regex_pattern.nil?
+                raise(ArgumentError, "missing column/s!") if not args.has_key?("COLUMNS")
+                keyRegEx = org.apache.hadoop.hbase.filter.RegexStringComparator.new(regex_pattern)
+                scan = org.apache.hadoop.hbase.client.Scan.new
+                scan.setFilter(org.apache.hadoop.hbase.filter.RowFilter.new(org.apache.hadoop.hbase.filter.CompareFilter::CompareOp::EQUAL, keyRegEx))
 
-		# Run the scanner
-		scanner = @table.getScanner(scan)
-		iterator = scanner.iterator
-		# Iterate results
-		while iterator.hasNext
-			row = iter.next
-			self.delete(row, args['COLUMNS'])
-			puts  "succesfully deleted row #{row}"
-		end
-	end
-
+                # Run the scanner
+                scanner = @table.getScanner(scan)
+                iterator = scanner.iterator
+                # Iterate results
+                while iterator.hasNext
+                        row = iterator.next
+                        row_string = java.lang.String.new(row.getRow)
+                        d = org.apache.hadoop.hbase.client.Delete.new(row.getRow, org.apache.hadoop.hbase.HConstants::LATEST_TIMESTAMP )
+                        @table.delete(d)
+                        puts  "succesfully deleted row #{row_string}"
+                end
+        end
 end
 
